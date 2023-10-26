bbba113737ff21f769d560796a67ae9b97a14d12
David Bašić <d.basic97.db@gmail.com>
2023-10-26 12:00:08 +0200

    first commit

---
+++ src/main/java/hr/dbasic/anephysiobe/AnePhysioBeApplication.java
@@ -0,0 +1,13 @@
+package hr.dbasic.anephysiobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnePhysioBeApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(AnePhysioBeApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/hr/dbasic/anephysiobe/AnePhysioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.dbasic.anephysiobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnePhysioBeApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
