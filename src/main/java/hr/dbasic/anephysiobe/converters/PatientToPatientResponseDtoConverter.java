217e5bc0c543a459b9bfa4362d1036bcf4e4c3b9
David Bašić <d.basic97.db@gmail.com>
2023-12-23 23:34:13 +0100

    slight dto adjustment

--- src/main/java/hr/dbasic/anephysiobe/converters/PatientToPatientResponseDtoConverter.java
+++ src/main/java/hr/dbasic/anephysiobe/converters/PatientToPatientResponseDtoConverter.java
@@ -2,6 +2,7 @@ package hr.dbasic.anephysiobe.converters;
 
 import hr.dbasic.anephysiobe.dto.responses.patientResponse.PRDoctorDto;
 import hr.dbasic.anephysiobe.dto.responses.patientResponse.PRPatientAddressDto;
+import hr.dbasic.anephysiobe.dto.responses.patientResponse.PRPatientOperationDto;
 import hr.dbasic.anephysiobe.dto.responses.patientResponse.PatientResponseDto;
 import hr.dbasic.anephysiobe.models.patients.Patient;
 import lombok.NonNull;
@@ -23,7 +24,12 @@ public class PatientToPatientResponseDtoConverter implements Converter<Patient, 
                 source.getSex(),
                 source.getLeadingMkb(),
                 source.getPatientMkbs(),
-                Objects.isNull(source.getOperations()) ? null : source.getOperations(),
+                source.getOperations().stream().map(op -> PRPatientOperationDto.builder()
+                        .id(op.getId())
+                        .procedureName(op.getProcedureName())
+                        .procedureDate(op.getProcedureDate())
+                        .build()
+                ).toList(),
                 source.getAdmissionDateTime(),
                 PRPatientAddressDto.builder()
                         .id(source.getId())
--- src/main/java/hr/dbasic/anephysiobe/dto/responses/patientResponse/PRPatientOperationDto.java
+++ src/main/java/hr/dbasic/anephysiobe/dto/responses/patientResponse/PRPatientOperationDto.java
@@ -1,7 +1,10 @@
 package hr.dbasic.anephysiobe.dto.responses.patientResponse;
 
+import lombok.Builder;
+
 import java.time.LocalDate;
 
+@Builder
 public record PRPatientOperationDto(
         String id,
         String procedureName,
